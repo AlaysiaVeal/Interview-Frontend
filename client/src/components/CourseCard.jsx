0b93e2169f9f848ba6396ec9cb3adff92d5d8719
Ben Kaplan <bjminkaplan@gmail.com>
2022-11-22 13:56:38 -0600

    base app complete

--- client/src/components/CourseCard.jsx
+++ client/src/components/CourseCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CourseCard = ({id, courseName }) => {
   return (
     <div className='course-card' key={id}>
-      <h2 id="zodiac-name">{courseName}</h2> 
+      <h2 id="course-name">{courseName}</h2> 
     </div>
   )
 }
